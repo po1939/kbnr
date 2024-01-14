5ea4d58dff8dac426d04d07080aaf678978002d5
hkim27 <71997934+hkim27@users.noreply.github.com>
2024-01-14 14:41:55 -0500

    mui installed

--- app/src/components/home/Home.tsx
+++ app/src/components/home/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MyComponent: React.FC = () => {
   return (
     <div>
-      <h1>Hello, this is MyComponent!</h1>
+      <h1>Hello, this is MyCosds!</h1>
       {/* Add your component content here */}
     </div>
   );
