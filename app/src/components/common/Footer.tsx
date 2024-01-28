6a4a75cb7bd20911d9f9afa10297a412c382d2dd
hkim27 <71997934+hkim27@users.noreply.github.com>
2024-01-27 22:36:41 -0500

    footer base code

--- app/src/components/common/Footer.tsx
+++ app/src/components/common/Footer.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import logo from './../../assets/images/kimnafooter.svg';
 
-const MyComponent: React.FC = () => {
+const Footer: React.FC = () => {
   return (
     <div>
-      <h1>Hello, this is MyComponent!</h1>
-      {/* Add your component content here */}
+      <footer className='footer'>
+        <div>
+        <nav>
+          <div className='logo'>
+            <a href='/'>
+              <img src={logo} />
+            </a>
+          </div>
+        </nav>
+      Business Hours <br></br>
+      <p>Mon:{'\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0'}10AM - 6:30PM</p>
+      </div>
+      </footer>
+      <div className="second-box">
+        Copyright &copy; 2024 DAALL HOUSE LLC
+      </div>
     </div>
+    
   );
 };
 
-export default MyComponent;
+export default Footer;
